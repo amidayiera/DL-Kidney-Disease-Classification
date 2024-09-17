39de003a2f6997b923e917f21dfa0a9976cad446
amidayiera <amidayiera@gmail.com>
2024-09-17 18:13:57 +0300

    create and update data_ingestion pipeline file

---
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,20 @@
+# update from data_ingestion notebook
+from cnnClassifier.config.configutation import ConfigurationManager
+from cnnClassifier.components.data_ingestion import DataIngestion
+from cnnClassifier import logger
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+         config = ConfigurationManager()
+    data_ingestion_config = config.get_data_ingestion_config()
+    data_ingestion = DataIngestion(config=data_ingestion_config)
+    data_ingestion.download_file()
+    data_ingestion.extract_zip_file()
+
