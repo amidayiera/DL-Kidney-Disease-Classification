10daf860377e8237eaaa3d68c78bec4f2a1d4d12
amidayiera <amidayiera@gmail.com>
2024-09-17 18:03:12 +0300

    update config file from data_ingestion notebook

---
+++ src/cnnClassifier/config/configutation.py
@@ -0,0 +1,33 @@
+# update config from data_ingestion notebook
+
+from cnnClassifier.constants import *
+from cnnClassifier.utils.common import read_yaml, create_directories
+
+class ConfigurationManager:
+    def __init__(  
+        self,
+        config_filepath = CONFIG_FILE_PATH, 
+        params_filepath = PARAMS_FILE_PATH):
+
+        self.config = read_yaml(config_filepath) #return a dict (configbox)
+        self.params = read_yaml(params_filepath)
+
+        create_directories([self.config.artifacts_root])  # create artifacts directory
+
+
+    # data_ingestion configuration
+    def get_data_ingestion_config(self) -> DataIngestionConfig:  #custom entity is defined as function return type
+        config = self.config.data_ingestion #extract from config.yaml 
+
+        create_directories([config.root_dir])
+
+        data_ingestion_config = DataIngestionConfig(
+            root_dir=config.root_dir,
+            source_URL=config.source_URL,
+            local_data_file=config.local_data_file,
+            unzip_dir=config.unzip_dir 
+        )
+
+        return data_ingestion_config
+    
+
