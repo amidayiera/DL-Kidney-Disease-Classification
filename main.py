e0f7647a94a9795e7bd867fcefef12cfc6da5c8b
amidayiera <amidayiera@gmail.com>
2024-09-17 18:24:04 +0300

    update main.py (WF8)

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-from src.cnnClassifier import logger
+#WF8: update main.py
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Custom Logging")
+# copy from data_ingestion pipeline file
+STAGE_NAME = "Data Ingestion Stage"
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx=======x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
