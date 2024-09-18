1cd455626c68fdcef925586e5bd0af0aae86fe28
amidayiera <amidayiera@gmail.com>
2024-09-18 23:25:53 +0300

    add base_model pipeline

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 #WF8: update main.py
 from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 # copy from data_ingestion pipeline file
 STAGE_NAME = "Data Ingestion Stage"
 if __name__ == '__main__':
     try:
-        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+        logger.info(f">>>>>>> started: {STAGE_NAME}  <<<<<<<")
         obj = DataIngestionTrainingPipeline()
         obj.main()
-        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx=======x")
+        logger.info(f">>>>>>> completed: {STAGE_NAME}  <<<<<<<\n\nx=======x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+# copy from prepare_base_model pipeline file
+STAGE_NAME = "Prepare Base Model Stage"
+if __name__ == '__main__':
+    try:
+        logger.info(f"\n\n>>>>>>> started: {STAGE_NAME}  <<<<<<<\n\n")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f"\n\n>>>>>>> completed: {STAGE_NAME}  <<<<<<<\n\nx=======x")
     except Exception as e:
         logger.exception(e)
         raise e
