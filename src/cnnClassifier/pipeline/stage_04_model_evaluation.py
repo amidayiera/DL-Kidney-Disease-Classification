ba7f1eacc8cca6a4426756a0c0a59702c034f838
amidayiera <amidayiera@gmail.com>
2024-09-22 16:42:49 +0300

    include comment about running with dvc

--- src/cnnClassifier/pipeline/stage_04_model_evaluation.py
+++ src/cnnClassifier/pipeline/stage_04_model_evaluation.py
@@ -16,7 +16,7 @@ class ModelEvaluationPipeline:
         eval_config = config.get_evaluation_config()
         evaluation = Evaluation(eval_config)
         evaluation.evaluation()
-        #evaluation.log_into_mlflow()
+        evaluation.log_into_mlflow() #comment out while running with dvc
 
 if __name__ == '__main__':
     try:
