2a56e091616e7966e16f1fd9ff50643aaca30a70
amidayiera <amidayiera@gmail.com>
2024-09-22 16:16:19 +0300

    update commands, include dvc option

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from flask import Flask, request, jsonify, render_template
 import os
-from flask_cors import CORS, cross_origin 
+from flask_cors import CORS, cross_origin
 from cnnClassifier.utils.common import decodeImage
 from cnnClassifier.pipeline.prediction import PredictionPipeline
-
-
 
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
 
 app = Flask(__name__)
 CORS(app)
-
 
 class ClientApp:
     def __init__(self):
@@ -25,15 +22,12 @@ def home():
     return render_template('index.html')
 
 
-
-
 @app.route("/train", methods=['GET','POST'])
 @cross_origin()
 def trainRoute():
-    os.system("python main.py")
-    # os.system("dvc repro")
+    os.system("python3 main.py")
+    # os.system("dvc repro") # to run with dvc pipeline
     return "Training done successfully!"
-
 
 
 @app.route("/predict", methods=['POST'])
